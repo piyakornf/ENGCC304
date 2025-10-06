f60cd4bf1843cb17b307441208c7450529da2da1
Zee <127277450+piyakornf@users.noreply.github.com>
2025-10-06 13:36:13 +0700

    Add files via upload

---
+++ Homework/Hw1/Hw1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int  Number1, Number2, Number3, Sum;
+
+    printf("กรอกหมายเลขที่ 1 ");
+    scanf("%d", &Number1);
+    
+    printf("กรอกหมายเลขที่ 2 ");
+    scanf("%d", &Number2);
+    
+    printf("กรอกหมายเลขที่ 3 ");
+    scanf("%d", &Number3);
+
+    Sum = Number1 + Number2 +Number3;
+
+    printf("ผลรวม = %d\n", sum);
+
+    return 0;
+}
